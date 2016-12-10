65a773a54ad3508ea71df5938fd1012c3f9fbaf6
Mohamed Lansari <mohamed.ml.me@gmail.com>
2016-12-10 15:43:04 -0500

    Day 9 Part 2 Part 2 works now.  Lots of struggle but it's working.

--- day9.py
+++ day9.py
@@ -1,7 +1,7 @@
 """
 Day 9 of Advent of Code
 Mohamed Lansari
-"""
+ """
 
 # main code goes here
 def main():
@@ -11,35 +11,62 @@ def main():
         com = f.read()
 
     # iterate through compressed
-    i, length = 0, 0
+    i, length, v1length = 0, 0, 0
     while len(com) > 0:
-        # split at first close parens
-        r = com.split(")", maxsplit=1)
-        key = r[0].strip("(")
-        key = key.split("x")
+        # split at the first open parentheses
+        r = com.split("(", maxsplit=1)
+        # check whether or not there actually were any more parens
+        if len(r) > 1:
+            key = r[1].split(")", maxsplit=1)
+            text = key[1]
+            info = key[0].split("x")
+            # join median text
+            length += len(r[0])             # increase the length by the amount prior to the recursion
+            decom = ''.join([decom, r[0]])
+            v1length += len(r[0])
 
-        # now we have the key, slice to fetch the string to repeat
-        rep = r[1][:int(key[0])]
-        length += recursCheckAmnt(rep, int(key[1]))
-        for i in range(int(key[1])):
-            decom = ''.join([decom, rep])
-        
-        com = r[1][int(key[0]):]
+            # with the key, recursively fetch the string to repeat it
+            slc = text[:int(info[0])]
+            length += recursCheckAmnt(slc, int(info[1]))
+            for i in range(int(info[1])):
+                decom = ''.join([decom, slc])
+                v1length += len(slc)
 
-    print("The length of the decompressed file is", len(decom), "in version 1")
-    print(length)
+            com = text[int(info[0]):] 
+        else:
+            decom = ''.join([decom, com])
+            v1length += len(com)
+            length += len(com)
+            com = ""
 
+    print("The length of the decompressed file is", v1length, "in version 1")
+    print("The length of the decompressed file is", length, "in version 2")
+
+# after much struggle, this worked properly for part 2
 def recursCheckAmnt(strng, amnt):
+    with open('out/day9out.txt', 'a') as f:
+        f.write("Checking " + strng + " with iterations of " + str(amnt) + "\n")
     # check if it has another iteration
-    if "(" in strng:
-        # then split and recurse
-        r = strng.split("(", maxsplit=1)
-        key = r[1].split(")", maxsplit=1)
-        print(key)
-        new = key[1]
-        key = key[0].split("x")
-        return amnt * (len(r[0]) + recursCheckAmnt(new, int(key[1])))
-    else:
-        return amnt * len(strng)
+    rAmount = 0
+    while len(strng) > 0:
+        if "(" in strng:
+            # then split and recurse
+            r = strng.split("(", maxsplit=1)
+            key = r[1].split(")", maxsplit=1)
+            new = key[1]                        # this is the new text
+            key = key[0].split("x")
+
+            passamnt = new[:int(key[0])]
+            restamnt = new[int(key[0]):]
+
+            rAmount += (len(r[0]) + recursCheckAmnt(passamnt, int(key[1])))
+            strng = restamnt
+        else:
+            rAmount += len(strng)
+            strng = ""
+    
+    with open('out/day9out.txt', 'a') as f:
+        f.write("Returning with a value of " + str(amnt * rAmount) + '\n')
+    return amnt * rAmount
 
 main()
